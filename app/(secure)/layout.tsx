38500baf7fe15c853b29fc592ecf9eb531074cfe
Gerhard Otto <gerrieotto123@gmail.com>
2025-04-01 23:11:41 +0200

    fix: :lipstick: improve ui

--- app/(secure)/layout.tsx
+++ app/(secure)/layout.tsx
@@ -7,9 +7,9 @@ export default function SecureLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section>
-      <Header/>
-      {children}
+    <section className="flex flex-col min-h-screen">
+      <Header />
+      <div className="grow">{children}</div>
       <Footer />
     </section>
   );
